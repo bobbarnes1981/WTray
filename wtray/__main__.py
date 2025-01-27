2eed68e11b5702c129ac0c5005a914235798a2fc
bobbarnes1981 <robertbarnes1981@googlemail.com>
2025-01-27 10:01:44 +0000

    Fecator

--- wtray/__main__.py
+++ wtray/__main__.py
@@ -10,26 +10,40 @@ class WTray(object):
         self.state = False
         menu = pystray.Menu(
             pystray.MenuItem('checked', self.__checked, checked=lambda item: self.state),
-            pystray.MenuItem('status', self.__status),
+            pystray.MenuItem('info', self.__info),
+            pystray.MenuItem('state', self.__state),
+            pystray.MenuItem('effects', self.__effects),
+            pystray.MenuItem('palettes', self.__palettes),
             pystray.MenuItem('on', self.__on),
             pystray.MenuItem('off', self.__off),
             pystray.MenuItem('exit', self.__exit))
         self.icon = pystray.Icon("WLED", Image.open("wtray/icon.ico"), menu=menu)
     def __checked(self, icon, item):
         self.state = not self.state
-    def __status(self, icon, item):
+    def __get(self, path):
         headers = {"Content-Type": "application/json"}
-        r = requests.get(f"{self.url}/json/state", headers=headers)
-        print(r.json())
-    def __on(self, icon, item):
-        headers = {"Content-Type": "application/json"}
-        r = requests.post(f"{self.url}/json/state", headers=headers, data=json.dumps({"on": True}))
+        r = requests.get(f"{self.url}/json/{path}", headers=headers)
         print(r)
         print(r.json())
-    def __off(self, icon, item):
+        return r.json()
+    def __post(self, path, data):
         headers = {"Content-Type": "application/json"}
-        r = requests.post(f"{self.url}/json/state", headers=headers, data=json.dumps({"on": False}))
+        r = requests.post(f"{self.url}/json/{path}", headers=headers, data=json.dumps(data))
+        print(r)
         print(r.json())
+        return r.json()
+    def __info(self, icon, item):
+        self.__get("info")
+    def __state(self, icon, item):
+        self.__get("state")
+    def __effects(self, icon, item):
+        self.__get("effects")
+    def __palettes(self, icon, item):
+        self.__get("palettes")
+    def __on(self, icon, item):
+        self.__post("state", {"on": True})
+    def __off(self, icon, item):
+        self.__post("state", {"on": False})
     def __exit(self, icon, item):
         icon.stop()
     def run(self):
